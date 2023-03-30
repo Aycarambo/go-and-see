55d3969bf2acdecebc32c617662668b6c67984be
Vigneron Thibault <thibault.vigneron@etudiant.univ-lr.fr>
2023-03-30 15:19:36 +0200

    v1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
+import { BonAchatsComponent } from "./pages/bon-achats/bon-achats.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BonAchatsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ client/src/app/model/bonAchat.ts
@@ -0,0 +1,6 @@
+export interface bonAchat {
+  id: number;
+  nom: string;
+  pourcentage: number;
+  prix: number;
+}
