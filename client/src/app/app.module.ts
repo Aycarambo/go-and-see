526d1f129c242019b36e5594d8edb401cb5feda6
Lafourcade Tanguy <tanguy.lafourcade@etudiant.univ-lr.fr>
2023-03-30 18:49:39 +0200

    calculDistance

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { BonAchatsComponent } from "./pages/bon-achats/bon-achats.component";
+import { CalculDistanceComponent } from "./components/calculDistance/calculDistance.component";
 
 @NgModule({
-  declarations: [AppComponent, BonAchatsComponent],
+  declarations: [AppComponent, BonAchatsComponent, CalculDistanceComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
