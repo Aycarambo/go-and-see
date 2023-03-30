ab0753eae3ef1d7a53d79ff8ec17fbb53d2c88aa
Thibault Favreau <tfavre@etudiant.univ-lr.fr>
2023-03-31 00:24:12 +0200

    v1 afficher adversaires

--- client/src/app/pages/map/map.component.ts
+++ client/src/app/pages/map/map.component.ts
@@ -3,6 +3,7 @@ import * as mapboxgl from "mapbox-gl";
 
 import { ArenesService } from "src/app/services/arenes.service";
 import { arene } from "src/app/model/arenes";
+import { connexionService } from "src/app/services/connexion.service";
 
 @Component({
   selector: "app-map",
@@ -14,12 +15,14 @@ export class MapComponent implements AfterViewInit {
   userLat: number;
   userLong: number;
   arenes: arene[] = [];
+  private markers: mapboxgl.Marker[] = [];
 
-  constructor(private arenesService: ArenesService) {}
+
+  constructor(private arenesService: ArenesService, private playerService: PlayerService, private connexionService: connexionService) {}
   ngOnInit(): void {}
 
   private initMap(): void {
-    if (navigator.geolocation) {
+    if (navigator.geolocation) { //Check si le navigateur autorise la geolocalisation
       navigator.geolocation.getCurrentPosition((position) => {
         this.userLat = position.coords.latitude;
         this.userLong = position.coords.longitude;
@@ -32,34 +35,56 @@ export class MapComponent implements AfterViewInit {
           center: [this.userLong, this.userLat],
           zoom: 10,
         });
-
-        const el = document.createElement("div");
-        el.className = "marker";
-        new mapboxgl.Marker(el);
-
-        this.arenesService.getArenes().subscribe((arenes) => {
-          this.arenes = arenes;
-
-          this.arenes.forEach((arene) => {
-            const el = document.createElement("div");
-            el.className = "marker-arene";
-            new mapboxgl.Marker(el)
-              .setLngLat([arene.long, arene.lat])
-              .addTo(homeMap);
-          });
-        });
-
-        const userMarker = document.createElement("div");
-        userMarker.className = "marker";
-        new mapboxgl.Marker(userMarker)
-          .setLngLat([this.userLong, this.userLat])
-          .addTo(homeMap);
-      });
-    } else {
+    }) ;
+  }
+    else{
       alert("La géolocalisation n'est pas prise en charge par ce navigateur.");
     }
   }
+  //affichage des markers (refresh tt les 5 minutes)
+  private displayUsers():void{
+    setInterval(() => {
+      //players markers
+      this.playerService.getUsers().subscribe((users) => {
+        // Supprimer les anciens marqueurs
+        this.markers.forEach((marker) => marker.remove());
+        this.markers = [];
+
+        // Ajouter les nouveaux marqueurs
+        const connectedPlayer = this.connexionService.me()
+        users.forEach((user) => {
+          const el = document.createElement("div");
+          if(user.id !== connectedPlayer.id){
+            el.className = "marker-adversaire";
+          }
+          else{
+            el.className = "marker";
+          }
+          const marker = new mapboxgl.Marker(el)
+            .setLngLat([user.longitude, user.latitude])
+            .addTo(this.homeMap);
+          this.markers.push(marker);
+        });
+      });
+      //arenas markers
+      this.arenesService.getArenes().subscribe((arenes) => {
+        this.arenes = arenes;
+
+        this.arenes.forEach((arene) => {
+          const el = document.createElement("div");
+          el.className = "marker-arene";
+          new mapboxgl.Marker(el)
+            .setLngLat([arene.long, arene.lat])
+            .addTo(homeMap);
+        });
+      });
+    }, 300000); // 5 minutes en millisecondes
+  } 
+  
+
   ngAfterViewInit(): void {
     this.initMap();
   }
 }
+
+
