4ed23f648da1b25d1489074989132998c0bef891
Ay_Carambo <stan.lucas07@gmail.com>
2023-03-30 15:29:02 +0200

    get arenes

---
+++ client/src/app/model/arenes.ts
@@ -0,0 +1,8 @@
+export interface arene {
+  id: number;
+  nom: string;
+  lat: number;
+  long: number;
+  joueurActif: number;
+  dateCapture: Date;
+}
---
+++ client/src/app/services/arenes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { arene } from "../model/arenes";
+
+import { map } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ArenesService {
+  constructor(private http: HttpClient) {}
+  private arenesPath = `${environment.apiUrl}/arenes?populate=*`;
+  private arenes: arene[] = [];
+
+  getArenes() {
+    return this.http.get(this.arenesPath).pipe(
+      map((data: any) => data.data),
+      map((fields: any) => {
+        fields.forEach((field: any) => {
+          this.arenes.push({
+            id: field.id,
+            nom: field.attributes.nom,
+            lat: field.attributes.lat,
+            long: field.attributes.long,
+            joueurActif: field.attributes.joueurActif.data.id,
+            dateCapture: field.attributes.dateCapture,
+          });
+        });
+
+        return this.arenes;
+      })
+    );
+  }
+}
