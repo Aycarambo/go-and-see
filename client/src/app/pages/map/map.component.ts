4ac8b26e02723968811a43aca8405b233d287e37
Lafourcade Tanguy <tanguy.lafourcade@etudiant.univ-lr.fr>
2023-03-31 12:36:09 +0200

    demo

--- client/src/app/pages/map/map.component.ts
+++ client/src/app/pages/map/map.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit } from "@angular/core";
 import * as mapboxgl from "mapbox-gl";
+import { HttpClient } from "@angular/common/http";
 
 import { ArenesService } from "src/app/services/arenes.service";
 import { arene } from "src/app/model/arenes";
@@ -26,6 +27,8 @@ export class MapComponent implements OnInit {
   user: any;
   serverUrl = environment.serverUrl;
   private map: mapboxgl.Map;
+  // isUpdatingMarker = false;
+  // markerUpdateInterval: any;
   private markers: markers = {
     userMarker: null,
     arenesMarkers: [],
@@ -37,7 +40,8 @@ export class MapComponent implements OnInit {
     private arenesService: ArenesService,
     private playerService: PlayersService,
     private connexionService: connexionService,
-    private data: DestinationService
+    private data: DestinationService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
@@ -58,13 +62,28 @@ export class MapComponent implements OnInit {
 
         setInterval(() => {
           navigator.geolocation.getCurrentPosition((position) => {
-            currentlong = position.coords.longitude;
-            currentLat = position.coords.latitude;
 
-            this.updateUserMarker(currentlong, currentLat);
+            // Pour les besoins de la démo cette fonctionalité a été commenté
+            // currentlong = position.coords.longitude;
+            // currentLat = position.coords.latitude;
+
+            // this.updateUserMarker(currentlong, currentLat);
             this.updateArenesMarkers();
           });
-        }, 1000);
+        }, 1000000000);
+
+
+        // Pour les besoins de la démo cette fonctionalité a été insérer ici, en cas contraire, elle est supprimé
+        navigator.geolocation.getCurrentPosition((position) => {
+          currentlong = position.coords.longitude;
+          currentLat = position.coords.latitude;
+
+          this.updateUserMarker(currentlong, currentLat);
+        });
+
+
+        // this.startMarkerUpdates();
+
       } else {
         alert(
           "La géolocalisation n'est pas prise en charge par ce navigateur."
@@ -91,7 +110,17 @@ export class MapComponent implements OnInit {
 
   updateUserMarker(long: number, lat: number) {
     this.markers.userMarker?.setLngLat([long, lat]);
+    console.log(this.markers.userMarker)
   }
+
+  // updateUserMarker(long: number, lat: number) {
+  //   if (!this.isUpdatingMarker) {
+  //     this.isUpdatingMarker = true;
+  //     this.markers.userMarker?.setLngLat([long, lat]);
+  //     this.isUpdatingMarker = false;
+  //   }
+  // }
+  
 
   initArenesMarkers() {
     this.arenesService.getArenes().subscribe((arenes) => {
@@ -192,4 +221,57 @@ export class MapComponent implements OnInit {
       zoom: 10,
     });
   }
+
+
+
+  // startMarkerUpdates() {
+  //   if (!this.markerUpdateInterval) {
+  //     this.markerUpdateInterval = setInterval(() => {
+  //       navigator.geolocation.getCurrentPosition((position) => {
+  //         const currentlong = position.coords.longitude;
+  //         const currentLat = position.coords.latitude;
+  
+  //         this.updateUserMarker(currentlong, currentLat);
+  //         this.updateArenesMarkers();
+  //       });
+  //     }, 1000);
+  //   }
+  // }
+  
+  // stopMarkerUpdates() {
+  //   if (this.markerUpdateInterval) {
+  //     clearInterval(this.markerUpdateInterval);
+  //     this.markerUpdateInterval = null;
+  //   }
+  // }
+  
+
+  fetchJsonData(): void {
+    this.http.get("assets/test.json").subscribe((data: any) => {
+      const etapes = data.etapes;
+      let currentIndex = 0;
+  
+      const updateMarker = () => {
+        if (currentIndex >= etapes.length) {
+          return;
+        }
+        this.updateUserMarker(
+          parseFloat(etapes[currentIndex].lon),
+          parseFloat(etapes[currentIndex].lat)
+        );        
+        currentIndex++;
+        setTimeout(updateMarker, 1000);
+      };
+  
+      updateMarker();
+    });
+  }
+
+  
+  
+  
 }
+
+
+
+
